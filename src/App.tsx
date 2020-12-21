d0b983c1f5ddf5359d0fdb43ca1b51ecce13e34f
hirouniverse <hiro.kikuchi.universe@gmail.com>
2020-12-21 18:00:46 +0700

    Add: button for chaging counter state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { FC } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementWithAmount, decrementWithAmount } from './redux/actions/counter';
 
 export const App: FC = () => {
+  const counter = useSelector<{ counter: number}, any>(state => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Hello World.</h1>
+      <h2>{ counter }</h2>
+      <div>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
+        <button onClick={() => dispatch(incrementWithAmount(2))}>increment 2</button>
+        <button onClick={() => dispatch(decrementWithAmount(2))}>decrement 2</button>
+      </div>
     </div>
   );
 }
